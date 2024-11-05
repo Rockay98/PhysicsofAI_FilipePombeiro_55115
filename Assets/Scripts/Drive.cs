3edb5657242c5a3a384ed94b40fd8f6f7bf40512
Rockay98 <filipepereira9855@gmail.com>
2024-11-05 12:39:03 +0000

    Physics of AI (Acceleration)

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -6,7 +6,9 @@ public class Drive : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
-
+    public Transform transGun;
+    public Transform gun;
+    public GameObject bulletObj;
     void Update()
     {
         // Get the horizontal and vertical axis.
@@ -24,5 +26,18 @@ public class Drive : MonoBehaviour
 
         // Rotate around our y-axis
         transform.Rotate(0, rotation, 0);
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            transGun.RotateAround(transGun.position, transGun.right, -0.5f);
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            transGun.RotateAround(transGun.position, transGun.right, 0.5f);
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(bulletObj, gun.position, gun.rotation);
+        }
     }
 }
